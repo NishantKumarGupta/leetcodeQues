07da44c38c2ece595a1dd9535726962eb1183012
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-01-02 17:41:50 +0530

    Time: 32 ms (54.03%), Space: 23.3 MB (24.51%) - LeetHub

--- pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -3,13 +3,10 @@ public:
     int numPairsDivisibleBy60(vector<int>& time) {
         int ans=0;
         unordered_map<int,int> mp;
-        for(auto &x:time){
-            x%=60;
-        }
+        for(auto &x:time)   x%=60;
+        
         for(auto x:time){
-            if(mp[60-x]>0){
-                ans+=mp[60-x];
-            }
+            if(mp[60-x]>0)  ans+=mp[60-x];
             mp[x]++;
         }
         if(mp[0]>1){
