9a09184b81b50d802d3269adf1fc9dabe29e4c5c
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-01-09 13:41:55 +0530

    Time: 108 ms (70.59%), Space: 35 MB (58.82%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -6,16 +6,12 @@ public:
             vector<bool> mat(n,false);
             vector<bool> matTwo(n,false);
             for(int j=0;j<matrix[i].size();j++){
+                if(mat[matrix[i][j]])
+                    return false;
+                if(matTwo[matrix[j][i]])
+                    return false;
                 mat[matrix[i][j]]=true;
                 matTwo[matrix[j][i]]=true;
-            }
-            for(int i=1;i<=matrix[0].size();i++){
-                if(!mat[i])
-                    return false;
-            }
-            for(int i=1;i<=matrix[0].size();i++){
-                if(!matTwo[i])
-                    return false;
             }
         }
         return true;
