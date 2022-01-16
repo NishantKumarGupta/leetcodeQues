cba53c5620d7267e4246d1ccc7c8a16e5636d7df
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-01-16 19:22:58 +0530

    Time: 16 ms (48.56%), Space: 19.6 MB (5.24%) - LeetHub

---
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        vector<int> forward;
+        vector<int> backward;
+        int count=100000,ans=0;
+        for(auto x:seats){
+            if(x)
+                count=0;
+            else
+                count++;
+            forward.push_back(count);
+        }
+        count=100000;
+        for(int i=seats.size()-1;i>=0;i--){
+            if(seats[i])
+                count=0;
+            else
+                count++;
+            backward.push_back(count);
+        }
+        reverse(backward.begin(),backward.end());
+        for(int i=0;i<seats.size();i++){
+            if(forward[i]){
+                ans=max(ans,min(forward[i],backward[i]));
+            }
+        }
+        return ans;
+    }
+};
