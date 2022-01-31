4c64767f4ee35534289bfb03f006608d380d3347
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-01-31 09:39:20 +0530

    Time: 372 ms (54.55%), Space: 288.4 MB (22.73%) - LeetHub

---
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> maxScoreIndices(vector<int>& nums) {
+        vector<int> left,right,ans;
+        int temp=0,ttemp=0,maxx=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            left.push_back(temp);
+            if(!nums[i])
+                temp++;
+            if(nums[i])
+                ttemp++;
+        }
+        left.push_back(temp);
+        for(int i=0;i<nums.size();i++){
+            right.push_back(ttemp);
+            if(nums[i])
+                ttemp--;
+        }
+        right.push_back(0);
+        for(int i=0;i<=nums.size();i++){
+            if(left[i]+right[i]>maxx){
+                maxx=left[i]+right[i];
+                ans.clear();
+                ans.push_back(i);
+            }
+            else if(left[i]+right[i]==maxx)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
