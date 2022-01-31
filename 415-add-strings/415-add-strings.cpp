9d50ea2d5b42b0d90398ddcb2c3351da68ecc371
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-01-31 10:47:08 +0530

    Time: 20 ms (24.35%), Space: 55.8 MB (20.42%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -1,44 +1,15 @@
 class Solution {
-public:
+public: //better implementation
     string addStrings(string num1, string num2) {
         string ans="";
-        int i=0,carry=0,n1l=num1.length()-1,n2l=num2.length()-1;
-        while(i<=n1l and i<=n2l){
-            int temp=(num1[n1l-i]-'0')+(num2[n2l-i]-'0')+carry;
-            carry=0;
-            if(temp>=10){
-                carry=temp/10;
-                temp%=10;
-            }
-            ans=to_string(temp)+ans;
-            i++;
+        int carry=0,n1l=num1.length()-1,n2l=num2.length()-1;
+        while(n1l>=0 or n2l>=0 or carry){
+            int v1=(n1l>=0)?num1[n1l--]-'0': 0;
+            int v2=(n2l>=0)?num2[n2l--]-'0': 0;
+            int temp=v1+v2+carry;
+            ans=to_string(temp%10)+ans;
+            carry=temp/10;
         }
-        if(i<=n1l){
-            while(i<=n1l){
-                int temp=(num1[n1l-i]-'0')+carry;
-                carry=0;
-                if(temp>=10){
-                    carry=temp/10;
-                    temp%=10;
-                }
-                ans=to_string(temp)+ans;
-                i++;
-            }
-        }
-        else if(i<=n2l){
-            while(i<=n2l){
-                int temp=(num2[n2l-i]-'0')+carry;
-                carry=0;
-                if(temp>=10){
-                    carry=temp/10;
-                    temp%=10;
-                }
-                ans=to_string(temp)+ans;
-                i++;
-            }
-        }
-        if(carry)
-            ans=to_string(carry)+ans;
         return ans;
     }
 };
