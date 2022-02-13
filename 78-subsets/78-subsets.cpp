f4049f268c45853a5ac0a7eb92047a4086fd7b9b
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-02-13 19:36:52 +0530

    Time: 8 ms (20.39%), Space: 16 MB (21.79%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void rec(vector<int> &nums,int itr,vector<int> curr){
+        if(itr>=nums.size()){
+            ans.push_back(curr);
+            curr.clear();
+            return;
+        }
+        rec(nums,itr+1,curr);
+        curr.push_back(nums[itr]);
+        rec(nums,itr+1,curr);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        ans.clear();
+        rec(nums,0,{});
+        return ans;
+    }
+};
