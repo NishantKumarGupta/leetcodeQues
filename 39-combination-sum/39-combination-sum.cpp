8fe0be7de03c2302081bf7fd8ff1fe596b861c3c
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-02-17 09:50:54 +0530

    Time: 201 ms (12.55%), Space: 62.5 MB (14.56%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    set<vector<int>> ansSet;
+    vector<vector<int>> ans;
     void rec(vector<int> &candidates,int target,vector<int> curr,int itr){
         //base case
         if(target==0){
-            ansSet.insert(curr);
+            ans.push_back(curr);
             return;
         }
         if(itr>=candidates.size() || target<0)
@@ -17,18 +17,16 @@ public:
             rec(candidates,target-candidates[itr],curr,itr);
             rec(candidates,target-candidates[itr],curr,itr+1);
         }
-        else{
+        else
             return;
-        }
     }
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        ansSet.clear();
-        vector<int> curr;
+        ans.clear();
         sort(candidates.begin(),candidates.end());
-        rec(candidates,target,curr,0);
-        vector<vector<int>> ans;
-        for(auto x:ansSet)
-            ans.push_back(x);
+        rec(candidates,target,{},0);
+        set<vector<int>> s( ans.begin(), ans.end() );
+        ans.assign( s.begin(), s.end() );
         return ans;
     }
 };
