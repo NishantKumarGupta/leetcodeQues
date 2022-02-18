ff9ac806929fbb0665cf81d755e042acac63f5d2
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-02-18 15:06:08 +0530

    Time: 3 ms (91.01%), Space: 7 MB (52.75%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> s;
+        for(int i=0; i<num.size(); ++i){
+           while(s.empty() == false && s.top()>num[i] && k>0){
+                //handling peak
+               s.pop();
+               k--;
+           } 
+            if(s.empty() && num[i]=='0'){
+               //if stack is empty but leading zero to be avoid
+               continue;
+           }
+           else{
+               //stack is empty 
+               //if the no is greater then top, simply push
+               s.push(num[i]);
+           } 
+        }
+        //cases to handle
+        // 1. If it is in ascending order
+        // 2. If we want to remove all elements[3rd case]
+        while(s.empty() == false && k>0){
+            s.pop();
+            k--;
+        }
+        //remove all elements
+        if(s.empty() == true){
+            string str ="0";
+            return str;
+        }
+        string str="";
+        while(s.empty() != true){
+            //top element will be removed [R->L]
+            str += s.top();
+            s.pop();           
+        }
+        reverse(str.begin(),str.end());
+        return str;
+    }
+};
