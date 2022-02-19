81ffc49358524c45f38932c0719028c7ef235211
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-02-19 16:58:28 +0530

    Time: 1698 ms (5.00%), Space: 48.9 MB (14.93%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int dp[10005];
     bool rec(vector<int>&nums,int itr){
-        if(itr==nums.size()-1)
+        if(itr>=nums.size()-1)
             return true;
-        if(itr>nums.size()-1)
-            return false;
+        // if(itr>nums.size()-1)
+        //     return false;
         if(dp[itr]!=-1)
             return dp[itr];
         for(int i=1;i<=nums[itr];i++)
