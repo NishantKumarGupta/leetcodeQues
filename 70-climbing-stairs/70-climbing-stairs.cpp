9990a01ce3c62c9d55d6e88dbc58ef6bc79a1732
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-02-19 14:21:37 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.03%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
-public://solution 1 memoization(top down) approach
+public://solution 2 bottom up(tabulation) approach
     int dp[50]={0};
     int climbStairs(int n) {
-        //base case
-        if(n==0)
-            return 1;
-        if(n<0)
-            return 0;
-        //dp case
         if(dp[n]>0)
             return dp[n];
-        //rec calls
-        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+        for(int i=0;i<=n;i++){
+            if(i==0 || i==1){
+                dp[i]=1;
+                continue;
+            }
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
