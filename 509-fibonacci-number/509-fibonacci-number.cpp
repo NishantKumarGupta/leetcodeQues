60aedddb4498e41cdfaa37d23ad79933b8f48e3d
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-02-19 13:54:37 +0530

    Time: 34 ms (5.06%), Space: 5.9 MB (79.88%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:// dp sol 1 memoization method
+    int dp[35]={0};
     int fib(int n) {
         //base case
         if(n==1 || n==0)
             return n;
+        //dp case
+        if(dp[n]>0)
+            return dp[n];
+        //rec cases
         return fib(n-1)+fib(n-2);
     }
 };
