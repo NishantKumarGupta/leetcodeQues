2a488752f8639cbea9d316d83b63134246280344
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-02-19 16:58:57 +0530

    Time: 970 ms (8.32%), Space: 49.2 MB (14.03%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,11 +1,9 @@
 class Solution {
-public:
+public:// solution 1: memoization apporach
     int dp[10005];
     bool rec(vector<int>&nums,int itr){
         if(itr>=nums.size()-1)
             return true;
-        // if(itr>nums.size()-1)
-        //     return false;
         if(dp[itr]!=-1)
             return dp[itr];
         for(int i=1;i<=nums[itr];i++)
