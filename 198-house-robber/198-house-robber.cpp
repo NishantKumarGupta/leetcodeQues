2f416f03216ccb34971ed99389585e1a45f71c84
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-02-19 15:41:53 +0530

    Time: 3 ms (51.84%), Space: 7.7 MB (46.67%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,17 +1,15 @@
 class Solution {
-public://solution 1: memoization(top down) approach
-    int dp[105];
-    int rec(vector<int>&nums,int itr){
-        //base case
-        if(itr>=nums.size())
-            return 0;
-        if(dp[itr]>-1)
-            return dp[itr];
-        //rec calls
-        return dp[itr]=max(rec(nums,itr+1),nums[itr]+rec(nums,itr+2));
-    }
+public://solution 1: bottom up(iterative/tabulation) approach
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return rec(nums,0);
+        int dp[105];
+        int n=nums.size();
+        if(n==1)
+            return nums[0];
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return max(dp[n-1],dp[n-2]);
     }
 };
