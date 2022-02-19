63212ed78da1e08b5485be4883bec9b24c33ecc4
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-02-19 17:16:19 +0530

    Time: 436 ms (25.58%), Space: 17.4 MB (14.08%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp[10005];
+    int rec(vector<int> &nums,int itr){
+        if(itr>=nums.size()-1)
+            return 0;
+        if(nums[itr]==0)
+            return 100000;
+        if(dp[itr]!=-1)
+            return dp[itr];
+        int ans=INT_MAX;
+        for(int i=1;i<=nums[itr];i++){
+            ans=min(ans,1+rec(nums,itr+i));
+        }
+        return dp[itr]=ans;
+    }
+    int jump(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return rec(nums,0);
+    }
+};
