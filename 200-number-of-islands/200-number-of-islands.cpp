aeca7551ea303cdcbe37348dcd6b05e0986b03cf
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-04-15 23:37:20 +0530

    Time: 32 ms (94.61%), Space: 18.2 MB (13.86%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public: //isko karenge hum BFS se ,nai hua isse DFS se karke dekhte
         //gaandu ques hai DFS me chalra BFS me TLE dera
-    void rec(vector<vector<char>> &grid,int i,int j){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j] =='0')
-            return;
-        grid[i][j]='0';
-        rec(grid,i+1,j);
-        rec(grid,i-1,j);
-        rec(grid,i,j+1);
-        rec(grid,i,j-1);
+        //retrying BFS
+    
+        bool isValid(vector<vector<char>> &grid,int i,int j){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size())
+            return false;
+        return true;
     }
     
     int numIslands(vector<vector<char>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-
+        
         int ans=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
                     ans++;
-                    rec(grid,i,j);
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        int x=q.front().first;
+                        int y=q.front().second;
+                        q.pop();
+                        grid[x][y]='0';
+                        if(isValid(grid,x+1,y) and grid[x+1][y]=='1')
+                            q.push({x+1,y}),grid[x+1][y]='0';
+                        if(isValid(grid,x-1,y) and grid[x-1][y]=='1')
+                            q.push({x-1,y}),grid[x-1][y]='0';
+                        if(isValid(grid,x,y-1) and grid[x][y-1]=='1')
+                            q.push({x,y-1}),grid[x][y-1]='0';
+                        if(isValid(grid,x,y+1) and grid[x][y+1]=='1')
+                            q.push({x,y+1}),grid[x][y+1]='0';
+                    }
                 }
             }
         }
