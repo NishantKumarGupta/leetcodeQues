5e3ba86e4b13f1c1711a68aa7e7fb20783109f0f
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-04-15 14:39:29 +0530

    Time: 75 ms (61.97%), Space: 23.7 MB (21.59%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    void DFS(vector<vector<int>> &grid,int i,int j){
+    //DFS done ez
+    //trying BFS
+    bool isValid(vector<vector<int>> &grid,int i,int j){
         if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size())
-            return;
-        if(!grid[i][j])
-            return;
-        grid[i][j]=0;
-        DFS(grid,i+1,j);
-        DFS(grid,i-1,j);
-        DFS(grid,i,j+1);
-        DFS(grid,i,j-1);
+            return false;
+        return true;
     }
     
     int numEnclaves(vector<vector<int>>& grid) {
@@ -18,8 +14,24 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]){
-                    if(!i or !j or i==m-1 or j==n-1)
-                        DFS(grid,i,j);
+                    if(!i or !j or i==m-1 or j==n-1){
+                        queue<pair<int,int>> q;
+                        q.push({i,j});
+                        while(!q.empty()){
+                            int x=q.front().first;
+                            int y=q.front().second;
+                            q.pop();
+                            grid[x][y]=0;
+                            if(isValid(grid,x+1,y) and grid[x+1][y])
+                                q.push({x+1,y}),grid[x+1][y]=0;
+                            if(isValid(grid,x-1,y) and grid[x-1][y])
+                                q.push({x-1,y}),grid[x-1][y]=0;
+                            if(isValid(grid,x,y+1) and grid[x][y+1])
+                                q.push({x,y+1}),grid[x][y+1]=0;
+                            if(isValid(grid,x,y-1) and grid[x][y-1])
+                                q.push({x,y-1}),grid[x][y-1]=0;
+                        }
+                    }
                 }
             }
         }
