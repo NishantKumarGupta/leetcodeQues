62082bda1fda043737fdf414c49f1ec60bda548e
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-04-17 22:50:55 +0530

    Time: 4 ms (70.88%), Space: 8.9 MB (5.30%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -4,12 +4,6 @@ public:
         int start=0,end=nums.size()-1;
         while(start<end){
             int mid=start+(end-start)/2;
-            if(mid==0 and nums[mid]>nums[mid+1])
-                return mid;
-            if(mid==nums.size()-1 and nums[mid]>nums[mid-1])
-                return mid;
-            if(nums[mid]>nums[mid+1] and nums[mid]>nums[mid-1])
-                return mid;
             if(nums[mid]>=nums[mid+1]){
                 end=mid;
             }
