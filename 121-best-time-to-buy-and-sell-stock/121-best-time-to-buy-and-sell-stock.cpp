6b2f610000d504588e4ddf2c9e78d255e6c91ed3
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-05-31 12:59:29 +0530

    Time: 156 ms (66.27%), Space: 93.4 MB (53.10%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
+    //for this 3 solutions come into mind. The first one being that we calculate a postfix sum array and store the next heighest number and for a single pass loop find the maximum difference
+    // The other one is a DP solution where we recursively travel the array and find out the maximum difference and use memoization for improving the time complexity
+    // The third one is the simple greedy method TC O(n) SC O(1)
+    
+    //writing the greedy solution first
     int maxProfit(vector<int>& prices) {
-        int ans=0,maxx=0,minn=INT_MAX;
-        for(int i=0;i<prices.size();i++){
-            if(prices[i]>=maxx)
-                maxx=prices[i];
-            if(prices[i]<=minn){
-                minn=prices[i];
-                maxx=0;
+        int minVal = INT_MAX, maxVal = INT_MIN, ans =0;
+        for(auto &element: prices){
+            if(element>maxVal){
+                maxVal = element;
             }
-            ans=max(ans,(maxx-minn));
-        } 
+            if(element < minVal){
+                minVal = element;
+                maxVal = 0;
+            }
+            ans = max(ans, maxVal-minVal);
+        }
         return ans;
     }
 };
