b6eda688f1eddc21d4bf9e5c3015116905f732b0
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-06-02 22:49:48 +0530

    Time: 1083 ms (83.23%), Space: 294.8 MB (68.52%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -5,16 +5,13 @@ public:
             return NULL;
         
         ListNode* slow = head, *fast = head,*prev = head;
-        
-        while(fast and fast -> next){
+        while(fast and fast -> next){ //finding the middle element
             if(slow != head)
                 prev = prev ->next;
             fast = fast -> next -> next;
             slow = slow-> next;
         }
-        
-        prev -> next = slow -> next;
-        
+        prev -> next = slow -> next; //deleting the middle element
         return head;
     }
 };
