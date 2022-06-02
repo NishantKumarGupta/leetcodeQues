a44ac3676444f9e2e450aa3fbfdbccbf79414c32
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-06-02 14:38:17 +0530

    Time: 33 ms (27.00%), Space: 19.6 MB (59.64%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,12 +1,21 @@
 class Solution {
-public:
+public: //SFI
+    /*
+        Method 1: Hash Table approach
+        Simply traverse the vector and increment the current value in the frequency
+        hash table and when frequency of an element crosses the majority line
+        we simply return it.
+        Tc - O(n) SC - O(n)
+    */
+    
+    //hash table approach
     int majorityElement(vector<int>& nums) {
-        int n=nums.size()/2;
-        unordered_map<int,int> mp;
-        for(auto x:nums){
-            mp[x]++;
-            if(mp[x]>n)
-                return x;
+        int majorityLine = nums.size()/2;
+        unordered_map<int,int> freqMap;
+        for(auto &element: nums){
+            freqMap[element]++;
+            if(freqMap[element]>majorityLine)
+                return element;
         }
         return -1;
     }
