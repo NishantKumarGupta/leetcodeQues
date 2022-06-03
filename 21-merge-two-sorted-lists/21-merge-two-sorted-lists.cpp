18fa7e354c28307017dfa68f2bfb3e9e8fa82647
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-06-03 14:19:56 +0530

    Time: 13 ms (36.83%), Space: 14.7 MB (81.92%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -5,46 +5,26 @@ public: //SFI
         We compare the current heads of the two linked list and add the smaller one
         to the answer linked list
         TC - O(n) SC - O(1)
+        
+        Method 2: Using recursion (shorter & cleaner code)
+        We call the function recursively to update the next pointer of the current
+        node
+        TC - O(n) SC - O(1)
     */
     
-    //two pointer approach
+    //recursive approach
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(!list1)
             return list2;
         if(!list2)
             return list1;
-        ListNode *newHead = (list1 -> val <= list2 -> val) ? list1 : list2;
-        if(newHead == list1)
-            list1 = list1 -> next;
-        else
-            list2 = list2 -> next;
-        ListNode *itr = newHead;
-        
-        while(list1 and list2){
-            if(list1 -> val <= list2 -> val){
-                itr -> next = list1;
-                list1 = list1 -> next;
-            }
-            else{
-                itr -> next = list2;
-                list2 = list2 -> next;
-            }
-            itr = itr -> next;
+        if(list1 -> val <= list2 -> val){
+            list1 -> next = mergeTwoLists(list1 -> next, list2);
+            return list1;
+        } 
+        else{
+            list2 -> next = mergeTwoLists(list1 , list2 -> next);
+            return list2;
         }
-        if(list1){
-            while(list1){
-                itr -> next = list1;
-                itr = itr -> next;
-                list1 = list1 -> next;
-            }
-        }
-        if(list2){
-            while(list2){
-                itr -> next = list2;
-                itr = itr -> next;
-                list2 = list2 -> next;
-            }
-        }
-        return newHead;
     }
 };
