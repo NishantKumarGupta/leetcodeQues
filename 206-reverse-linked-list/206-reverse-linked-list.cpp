dbfdef5f747b6e106f542de3edab008de9b4b558
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-06-07 20:29:13 +0530

    Time: 10 ms (42.53%), Space: 8.3 MB (80.33%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public: //SFI
     /*
-        Method 1: Greedy method
+        Method 1: Greedy method (Iterative)
         We will simply traverse the linked list and update the next pointers of the
         nodes to the previous nodes
         TC - O(n) SC - O(1)
     */
     
-    //greedy method
+    //greedy method (Iterative)
     ListNode* reverseList(ListNode* head) {
         ListNode* currNode = head, *prevNode = NULL;
         while(currNode){
