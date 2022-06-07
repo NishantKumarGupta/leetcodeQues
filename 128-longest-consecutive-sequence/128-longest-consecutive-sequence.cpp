57d726ba1dd5bb8621f4088c2e8ebc9373dcccc4
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-06-07 22:36:11 +0530

    Time: 1011 ms (10.59%), Space: 49.8 MB (30.14%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public: //SFI
+    /*
+        Method 1: HashMap approach
+        We insert every element of nums into an ordered map. and starting from the 
+        minimum value we check for the longest consecutive sequence and return it.
+        TC - O(nlogn) SC - O(n)
+        
+        Method 2: Union Find method
+        direct question of union find
+        TC - O(n) SC - O(n)
+        
+        Method 3: Optimal HashSet approach
+        In this we insert all elements in the hashset and iterate over the array again
+        2 cases may arise
+        1. current - 1 exist in hashset => do nothing
+        2. current - 1 does not exist => find the hashset for the upper values
+        TC - O(n) SC - O(n)
+    */
+    
+    //optimal Hashset approach
+    int longestConsecutive(vector<int>& nums) {
+        int ans = 0;
+        unordered_set<int> hashSet;
+        for(auto &element:nums)
+            hashSet.insert(element);
+        
+        for(auto &element:nums){
+            if(hashSet.find(element-1) == hashSet.end()){
+                int mover = element;
+                while(hashSet.find(mover) != hashSet.end())
+                    mover++;
+                ans = max(ans, (mover-element));
+            }
+        }
+        return ans;
+    }
+};
