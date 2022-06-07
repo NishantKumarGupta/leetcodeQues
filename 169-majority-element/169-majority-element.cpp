c97b99d7a7bce3d0c4c0a23b2755fbcfbd5e8b64
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-06-07 19:56:51 +0530

    Time: 40 ms (12.70%), Space: 19.6 MB (13.92%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -6,16 +6,34 @@ public: //SFI
         hash table and when frequency of an element crosses the majority line
         we simply return it.
         Tc - O(n) SC - O(n)
+        
+        Method 2: Moore Voting algorithm
+        The intution behind this algorithm is if we have an element with occurance >n/2
+        then the rest of elements combined will have occurance of <n/2 thus the other
+        elements will never be able to outvote the majority element.
+        In this we take an element and if the present element is equal to candidate 
+        element we decrease the votes otherwise we increment the votes
+        TC O(n) SC - O(1)
     */
     
-    //hash table approach
+    //moore voting algo
     int majorityElement(vector<int>& nums) {
-        int majorityLine = nums.size()/2;
-        unordered_map<int,int> freqMap;
-        for(auto &element: nums){
-            freqMap[element]++;
-            if(freqMap[element]>majorityLine)
-                return element;
+        int candidate = -1, votes = 0;
+        for(auto &x:nums){
+            if(votes == 0)
+                candidate = x;
+            
+            if(candidate == x)
+                votes++;
+            else
+                votes--;
+        }
+        int count = 0;
+        for(auto &element:nums){
+            if(element == candidate)
+                count++;
+            if(count>(int)nums.size()/2)
+                return candidate;
         }
         return -1;
     }
