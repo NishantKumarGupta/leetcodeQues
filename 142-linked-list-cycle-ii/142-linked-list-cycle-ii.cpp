3d4514727b2511b10d6384a5a8767a32b7faf4c0
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-06-08 12:03:48 +0530

    Time: 12 ms (51.65%), Space: 7.5 MB (89.82%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -1,28 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
+class Solution { //SFI
 public:
+    /*
+        Method 1: Floyd's cycle detection
+        In this we have two pointers and traverse one pointer 2 places and the other
+        1 place and if they meet we take the slow pointer at head and move both
+        one place at a time, the next time they meet is the node where the cycle
+        begins.
+        TC - O(n) SC - O(1)
+    */
+    
+    //floyd's cycle detection
     ListNode *detectCycle(ListNode *head) {
-        if(head==NULL)
-            return NULL;
-        ListNode* fast=head;
-        ListNode* slow=head;
-        while(fast!=NULL and fast->next!=NULL){
-            fast=fast->next->next;
-            slow=slow->next;
-            if(fast==slow){
-                slow=head;
-                while(fast!=slow){
-                    fast=fast->next;
-                    slow=slow->next;
+        ListNode *fast = head, *slow = head;
+        while(fast and fast -> next){
+            fast = fast -> next -> next;
+            slow = slow -> next;
+            if(fast == slow){
+                slow = head;
+                while(fast != slow){
+                    fast = fast -> next;
+                    slow = slow -> next;
                 }
-            return fast;
+                return fast;
             }
         }
         return NULL;
