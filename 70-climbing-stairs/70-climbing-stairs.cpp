81135b3e87e755a69ca6396e0b21a73d537bb8ec
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-06-23 10:36:44 +0530

    Time: 0 ms (100.00%), Space: 6 MB (39.43%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -5,10 +5,15 @@ public://solution 1 top down (recursive dp)
     
     int climbStairs(int n) {
         vector<int> dp(46, -1);
-        dp[0] = 1, dp[1] = 1;
+        if(n == 0 || n == 1)
+            return 1;
+        int secondLast = 1, last = 1;
+        int curr;
         for(int i = 2; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+            secondLast = last;
+            last = curr;
+            curr = secondLast + last;
         }
-        return dp[n];
+        return curr;
     }
 };
