6606cd4aa2d3bc776b31182fbaf124d1e2079711
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-06-27 13:45:56 +0530

    Time: 46 ms (45.93%), Space: 13.3 MB (99.55%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        int maxx=0;
-        for(auto c:n){
-            if((c-'0')>maxx)
-                maxx=c-'0';
-        }
-        return maxx;
+        // int maxx=0;
+        // for(auto c:n){
+        //     if((c-'0')>maxx)
+        //         maxx=c-'0';
+        // }
+        return *max_element(n.begin(),n.end()) - '0';
     }
 };
