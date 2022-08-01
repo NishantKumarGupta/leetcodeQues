bc68a358344fcb1a35e0a1d429cb1e5aae77ff8f
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-08-01 09:12:47 +0530

    Time: 7 ms (95.91%), Space: 10.1 MB (53.11%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,20 +1,22 @@
+//SFIP
 class Solution {
 public:
-    int minSum(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp){
-        if(i >= grid.size() || j >= grid[0].size()){
-            return 4000001;
-        }
-        if(i == (int)grid.size() - 1 && j == (int)grid[0].size() - 1)
-            return grid[i][j];
-        
-        if(dp[i][j] > -1)
-            return dp[i][j];
-        
-        return dp[i][j] = grid[i][j] + min(minSum(grid, i + 1, j, dp), minSum(grid, i, j + 1, dp));
-    }
-    
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(201, vector<int>(201, -1));
-        return minSum(grid, 0, 0, dp);
+        int row = grid.size(), col = grid[0].size();
+        vector<vector<int>> dp(row, vector<int>(col, 0));
+        dp[0][0] = grid[0][0];
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(i == 0 and j == 0)
+                    continue;
+                if(i == 0)
+                    dp[i][j] = grid[i][j] + dp[i][j - 1];
+                else if(j == 0)
+                    dp[i][j] = grid[i][j] + dp[i - 1][j];
+                else
+                    dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[row - 1][col - 1];
     }
 };
