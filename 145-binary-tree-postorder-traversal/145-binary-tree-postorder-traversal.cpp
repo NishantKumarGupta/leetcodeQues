73142989471485872523385ed08ddeac037292c5
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-08-28 17:33:59 +0530

    Time: 4 ms (41.97%), Space: 8.3 MB (86.85%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -16,19 +16,33 @@ public:
         
         if(root == NULL) return ans;
         
-        stack<TreeNode *> st1, st2;
-        st1.push(root);
-        while(!st1.empty()){
-            root = st1.top();
-            st1.pop();
-            st2.push(root);
-            if(root -> left != NULL) st1.push(root -> left);
-            if(root -> right != NULL) st1.push(root -> right);
+        stack<TreeNode *> st;
+        
+        TreeNode *curr = root;
+        
+        while(curr != NULL || !st.empty()){
+            if(curr != NULL){
+                st.push(curr);
+                curr = curr -> left;
+            }
+            else{
+                TreeNode *temp = st.top() -> right;
+                if(temp == NULL){
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp -> val);
+                    while(!st.empty() && temp == st.top() -> right){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp -> val);
+                    }
+                }
+                else{
+                    curr = temp;
+                }
+            }
         }
-    while(!st2.empty()){
-        ans.push_back(st2.top() -> val);
-        st2.pop();
-    }
-    return ans;
+        
+        return ans;
     }
 };
