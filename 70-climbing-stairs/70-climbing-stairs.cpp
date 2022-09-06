2dfcd0786f0417a86388c46936239139eaf99f26
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-09-06 12:00:46 +0530

    Time: 3 ms (36.09%), Space: 6.1 MB (31.67%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,17 +2,16 @@ class Solution {
 public://solution 1 top down (recursive dp)
        //solution 2 bottom up (tabulation dp)
         // space optimization
-    
-    int climbStairsHelper(int n, vector<int> &dp){
-        if(n == 0 || n == 1) return 1;
-        
-        if(dp[n] != -1) return dp[n];
-        
-        return dp[n] = climbStairsHelper(n - 1, dp) + climbStairsHelper(n - 2, dp);
-    }
+
     
     int climbStairs(int n) {
         vector<int> dp(n + 1, -1);
-        return climbStairsHelper(n, dp);
+        dp[0] = 1, dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
     }
 };
