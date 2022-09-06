df8a57f527ea0ed0ae6d26ff3f8511c91c0fbd09
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-09-06 16:32:34 +0530

    Time: 4 ms (36.04%), Space: 7.7 MB (49.18%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,13 +4,14 @@ public:
         int n = nums.size();
         if(n == 1)
             return nums[0];
-        vector<int> dp(n, -1);
-        dp[0] = nums[0], dp[1] = max(nums[1], nums[0]);  
+        int secondPrev = nums[0], prev = max(nums[1], nums[0]);  
         
         for(int i = 2; i < n; i++){
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+            int curr = max(nums[i] + secondPrev, prev);
+            secondPrev = prev;
+            prev = curr;
         }
         
-        return dp[n - 1];
+        return prev;
     }
 };
