c613c05a2b6e80e66a9bc9d8504ddd46f65da068
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-09-06 11:44:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.91%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,16 +1,15 @@
 class Solution {
-public:// dp sol 2 bottom up(tabulation) method
-    int dp[35]={0};
+public:
     int fib(int n) {
-        if(dp[n]>0)
-            return dp[n];
-        for(int i=0;i<=n;i++){
-            if(i==1 || i==0){
-                dp[i]=i;
-                continue;
-            }
-            dp[i]=dp[i-1]+dp[i-2];
+        if(n <= 1)
+            return n;
+        
+        int prev = 1, secondPrev = 0;
+        for(int i = 2; i <= n; i++){
+            int curr = prev + secondPrev;
+            secondPrev = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
