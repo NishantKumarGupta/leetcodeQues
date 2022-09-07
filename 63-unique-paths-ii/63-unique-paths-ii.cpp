354ddf005cdbb3f6ae14fba4549cbcf768e12b6f
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-09-07 15:31:16 +0530

    Time: 7 ms (40.46%), Space: 7.7 MB (67.03%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -9,24 +9,44 @@ public:
 //         Method 2: DP iterative TC - O(n * m) SC - O(n * m)
     
     
-    int uniquePaths(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp){
-        if(m < 0 || n < 0 || obstacleGrid[m][n] == 1)
-            return 0;
-        if(m == 0 && n == 0)
-            return 1;
+//     int uniquePaths(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp){
+//         if(m < 0 || n < 0 || obstacleGrid[m][n] == 1)
+//             return 0;
+//         if(m == 0 && n == 0 && obstacleGrid[i][j] == 0)
+//             return 1;
    
-        if(dp[m][n] != -1) return dp[m][n];
+//         if(dp[m][n] != -1) return dp[m][n];
         
-        int goUp = uniquePaths(m - 1, n, obstacleGrid, dp);
-        int goLeft = uniquePaths(m, n - 1, obstacleGrid, dp);
+//         int goUp = uniquePaths(m - 1, n, obstacleGrid, dp);
+//         int goLeft = uniquePaths(m, n - 1, obstacleGrid, dp);
         
-        return dp[m][n] = goUp + goLeft;
-    }
+//         return dp[m][n] = goUp + goLeft;
+//     }
     
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         vector<vector<int>> dp(m, vector<int> (n, -1));
-        return uniquePaths(m - 1, n - 1, obstacleGrid, dp);
+        
+        for(int i = 0; i < m; i ++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    if(obstacleGrid[i][j] == 0)
+                        dp[i][j] = 1;
+                    else
+                        dp[i][j] = 0;
+                }
+                else if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else{
+                    int goDown = 0, goRight = 0;
+                    if(i > 0) goDown = dp[i - 1][j];
+                    if(j > 0) goRight = dp[i][j - 1];
+                    dp[i][j] = goDown + goRight;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
