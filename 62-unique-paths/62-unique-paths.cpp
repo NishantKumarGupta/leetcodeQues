13bdf9f2335158a50bb53e8c73975e0d792a61da
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-09-07 14:39:01 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (53.44%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -5,9 +5,11 @@ public: //SFI
         We simply move to either right or down and consider this as our starting position
         and try to reach the goal position and return it to solve the bigger problem
         and apply DP.
-        TC - O(n*m) SC - O(n*m)
+        TC - O(n*m) SC - O(n*m) + O(n * m)
         
-        Method 2: Using combinatrics
+        Method 2: DP iterative TC - O(n * m) SC - O(n*m)
+        
+        Method 3: Using combinatrics
         When we observe the paths we find out 2 key observations
         1. All unique paths have same length
         2. All paths have same number of right and down steps
@@ -16,22 +18,38 @@ public: //SFI
     */
     
     //DP approach
-    int numPaths(int posRow, int posCol, int m, int n, vector<vector<int>> &dp){
-        if(posRow> m || posCol > n)
-            return 0;
-        if(posRow == m and posCol == n)
-            return 1;
+//     int numPaths(int m, int n, vector<vector<int>> &dp){
+//         if(m < 0 || n < 0)
+//             return 0;
+//         if(m == 0 && n == 0)
+//             return 1;
         
-        if(dp[posRow][posCol]>-1)
-            return dp[posRow][posCol];
+//         if(dp[m][n] != -1) return dp[m][n];
         
-        int right = numPaths(posRow, posCol+1, m, n,dp);
-        int down = numPaths(posRow+1, posCol, m, n,dp);
-        return dp[posRow][posCol] = right + down;
-    }
+//         int goLeft = numPaths(m, n - 1, dp);
+//         int goUp = numPaths(m - 1, n, dp);
+        
+//         return dp[m][n] = goLeft + goUp;
+//     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+2, vector<int>(n+2,-1));
-        return numPaths(1,1,m,n,dp);
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        dp[0][0] = 1;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) continue;
+                    int goRight = 0, goDown = 0;
+                    if(i > 0){
+                        goDown = dp[i - 1][j];
+                    }
+                    if(j > 0){
+                        goRight = dp[i][j - 1];
+                    }
+                    dp[i][j] = goDown + goRight;
+                
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
