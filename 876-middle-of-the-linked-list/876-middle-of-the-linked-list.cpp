c7b7f0dd674f5dcaeba2acc2a46ca7e57dfa717a
Nishant Kumar Gupta <67021346+NishantKumarGupta@users.noreply.github.com>
2022-09-18 18:09:58 +0530

    Time: 3 ms (59.42%), Space: 7.1 MB (22.19%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -1,23 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-public: //SFI
-    /*
-        Method 1: Brute force approach
-        We first traverse the whole linked list and find the number of nodes in the
-        linked list. Then we traverse once again and stop and the middle of the size
-        TC - O(n) SC - O(1)
-        
-        Method 2: Two Pointer approach/ Slow Fast Pointer appraoch
-        We take two pointers and traverse the linked list as fast increments 2 nodes
-        at a time and the slow pointer traverses 1 node at a time and when the fast
-        pointer reaches the end of the linked list the slow pointer will be at the 
-        middle node of the linked list
-        TC  - O(n) SC - O(1)
-    */
-    
-    //two pointer approach
+public:
     ListNode* middleNode(ListNode* head) {
-        ListNode *fast = head , *slow = head;
-        while(fast and fast -> next){
+        ListNode *fast = head, *slow = head;
+        while(fast != NULL && fast -> next != NULL){
             fast = fast -> next -> next;
             slow = slow -> next;
         }
